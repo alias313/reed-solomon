de9153d67deb87ec2f5b678c8ec8478e09ae16ec
Elies <elisgarciaalvira@gmail.com>
2025-01-30 05:52:34 +0100

    Added simple decoding function

--- wikiversity/qr_formatting.py
+++ wikiversity/qr_formatting.py
@@ -48,6 +48,26 @@ def qr_check_format(fmt: int, debug: int = 1) -> int:
                 print(f"Result       :\t\t {fmt:015b}")
     return fmt
 
+def hamming_weight(x):
+   weight = 0
+   while x > 0:
+      weight += x & 1
+      x >>= 1
+   return weight
+
+def qr_decode_format(fmt):
+   best_fmt = -1
+   best_dist = 15
+   for test_fmt in range(0,32):
+      test_code = (test_fmt<<10) ^ qr_check_format(test_fmt<<10)
+      test_dist = hamming_weight(fmt ^ test_code)
+      if test_dist < best_dist:
+         best_dist = test_dist
+         best_fmt = test_fmt
+      elif test_dist == best_dist:
+         best_fmt = -1
+   return best_fmt
+
 
 print("Example format checked: " + bin(qr_check_format(0b000111101011001, debug=0)))
 
