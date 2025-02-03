bb7d81cdb6530dcac2e9a9074ad20fa56797207e
Elies <elisgarciaalvira@gmail.com>
2025-02-03 22:32:46 +0100

    Russian peasant ftw

---
+++ wikiversity/noLUT_russian_peasant.py
@@ -0,0 +1,12 @@
+def gf_mult_noLUT(x, y, prim=0, field_charac_full=256, carryless=True):
+    '''Galois Field integer multiplication using Russian Peasant Multiplication algorithm (faster than the standard multiplication + modular reduction).
+    If prim is 0 and carryless=False, then the function produces the result for a standard integers multiplication (no carry-less arithmetics nor modular reduction).'''
+    r = 0
+    while y: # while y is above 0
+        if y & 1: r = r ^ x if carryless else r + x # y is odd, then add the corresponding x to r (the sum of all x's corresponding to odd y's will give the final product). Note that since we're in GF(2), the addition is in fact an XOR (very important because in GF(2) the multiplication and additions are carry-less, thus it changes the result!).
+        y = y >> 1 # equivalent to y // 2
+        x = x << 1 # equivalent to x*2
+        if prim > 0 and x & field_charac_full: x = x ^ prim # GF modulo: if x >= 256 then apply modular reduction using the primitive polynomial (we just subtract, but since the primitive number can be above 256 then we directly XOR).
+
+    return r
+
